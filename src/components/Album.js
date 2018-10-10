bd1c2e0934d8e0c0cb512ba3102bcb98de610c33
diva-D <davidgentile@gmail.com>
2018-10-10 12:58:41 -0400

    assignment components album completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -35,6 +35,15 @@ class Album extends Component {
                         <col id="song-duration-column" />
                     </colgroup>
                     <tbody>
+                        {this.state.album.songs.map( (song, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{song.title}</td>
+                                    <td>{song.duration}</td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             </section>
